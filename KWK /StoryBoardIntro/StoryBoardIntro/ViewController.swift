38952228a15ec179b84b21cc0d10a8a3d7e7b5aa
brit1172 <baparicio.16@gmail.com>
2020-08-05 11:33:50 -0400

    Add files via upload

---
+++ KWK /StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Britney Aparicio on 8/5/20.
+//  Copyright © 2020 Britney Aparicio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statment-testing testing!")
+    }
+
+
+}
+
